6749fe6615a6b6c5b9bd7e85282d8df4ea33ca23
Jamie Davis <jamjam@umich.edu>
2019-10-17 10:31:14 -0400

    Create remove-duplicate-ids.js

---
+++ consolidated_records/remove-duplicate-ids.js
@@ -0,0 +1,76 @@
+// ==UserScript==
+// @name         Remove Duplicate IDs
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @updateURL    https://github.com/jamie-r-davis/slate_userscripts/consolidated_records/remove-duplicate_ids.js
+// @description  try to take over the world!
+// @author       You
+// @match        https://enrollmentconnect.umich.edu/manage/database/dedupe
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+    async function GetDuplicates(folder) {
+        var url = new URL('https://enrollmentconnect.umich.edu/manage/query/run?id=de498e93-ce6a-4dc3-a2b1-69412acc79c6&h=a919c3f6-8a58-d14a-014c-68cf290fde38&cmd=service&output=json');
+        url.searchParams.append('folder', folder);
+        console.log(url);
+        let duplicates = await fetch(url).then(async function(resp) {let data = await resp.json(); console.log(data); return data.row})
+        return duplicates
+    }
+
+    async function RemoveDuplicates() {
+        var folder = View.folder;
+        console.log(`Folder is ${folder}`);
+        let duplicates = await GetDuplicates(folder);
+        for (var i=0; i<duplicates.length; i++) {
+            var duplicate = duplicates[i];
+            var el = document.querySelector(`tr#r${duplicate.id}`);
+            if (el) {
+                el.remove();
+            }
+        }
+    }
+
+    class View {
+        static get folder() {
+            let container = document.querySelector('#responsesContainer');
+            if (container) {
+                return document.querySelector('#responsesContainer input[name="folder"]').value;
+            }
+            return null;
+        }
+        static get dataset() {
+            let container = document.querySelector('#responsesContainer');
+            if (container) {
+                return document.querySelector('#responsesContainer input[name="dataset"]').value;
+            }
+            return null;
+        }
+        static get scope() {
+            let container = document.querySelector('#responsesContainer');
+            if (container) {
+                let node = document.querySelector('#responsesContainer input[name="scope"]');
+                if (node) {
+                    return node.value;
+                } else {
+                    return 'person';
+                }
+            }
+            return null;
+        }
+    }
+
+    function AddBtn(text, event) {
+        let btn = document.createElement('button');
+        btn.innerText = text;
+        btn.addEventListener('click', event);
+        btn.style['margin-right'] = '0.5rem';
+        let parent = document.querySelector('#content');
+        parent.insertBefore(btn, parent.firstChild);
+    }
+
+    AddBtn('Remove Dup IDs', RemoveDuplicates);
+})();
